15aca91bad4282537840e8c4dcba6d078f20f966
aiden-perkins <perkinsaidenm@gmail.com>
2023-11-05 02:28:36 -0800

    problem 4 done in a cleaner, better way

--- 2023/4/main.py
+++ 2023/4/main.py
@@ -1,71 +1,51 @@
-import sys
 import math
 
-p = False
-participants = []
-tt = []
-for line in sys.stdin:
-    line = line.replace('\n', '').split(' ')
-    if not p:
-        p, m, b, s = [int(a) for a in line]
-    elif len(participants) < p:
-        line = [int(a) for a in line]
-        for i in range(0, len(line), 2):
-            # Index 0 is ID, 1 is their age, 2 is shares, 3 is if their shares should count towards the dead pool,
-            # and 4 is if they are alive or not. This would be so much easier to read if I used OOP.
-            participants.append([len(participants) + 1, s - line[i], line[i + 1], True, True])
-        if len(participants) == p:  # Making the tontines
-            participants.sort(key=lambda a: a[1])
-            c_a = 50
-            t = []
-            for person in participants:
-                if person[1] in range(c_a, c_a + b):
-                    t.append(person)
-                else:
-                    # This extra append is to keep track of the dead shares & how many people are alive.
-                    t.append([0, len(t)])
-                    tt.append(t)
-                    t = [person]
-                    c_a += b
-            t.append([0, len(t)])
-            tt.append(t)
+p, m, b, s = [int(a) for a in input().split(' ')]
+people = {}
+while len(people) < p:
+    line = [int(a) for a in input().split(' ')]
+    for i in range(0, len(line), 2):
+        people[len(people) + 1] = [s - line[i], line[i + 1]]
+age_groups = {}
+for p_id in people:
+    person = people[p_id]
+    group = ((person[0] - 50) // b) + 1
+    if group not in age_groups:
+        age_groups[group] = [
+            person[1],  # This is the total shares that the investors have.
+            0,  # This is the total shares that will be distributed to the other investors.
+            [p_id]  # This list is the people alive in the tontine, so we know when to stop payouts.
+        ]
     else:
-        pct, died, who = None, None, []
-        line = [float(a) for a in line]
-        for idx in range(len(line) + 1):
-            if pct is None:
+        age_groups[group][0] += person[1]
+        age_groups[group][2].append(p_id)
+while True:
+    pct, died, who = None, None, []
+    try:
+        line = [float(a) for a in input().split(' ')]
+    except EOFError:
+        break
+    for idx in range(len(line) + 1):
+        if pct is None:
+            pct = line[idx]
+        elif died is None:
+            died = int(line[idx])
+        elif len(who) < died:
+            who.append(int(line[idx]))
+        else:
+            # This is when we actually calculate the percentage for the payout.
+            for group_number in age_groups:
+                for dead_person in who:
+                    if dead_person in age_groups[group_number][2]:
+                        if len(age_groups[group_number][2]) > m:
+                            shares = people[dead_person][1]
+                            age_groups[group_number][0] -= shares
+                            age_groups[group_number][1] += shares
+                        age_groups[group_number][2].remove(dead_person)
+                if len(age_groups[group_number][2]) > 0:
+                    raw = ((age_groups[group_number][1] * pct) / age_groups[group_number][0]) + pct
+                    print(f'{s} {group_number} {math.trunc(int(raw * 1000) / 10) / 100:.2f}')
+            s += 1
+            pct, died, who = None, None, []
+            if len(line) != idx:
                 pct = line[idx]
-            elif died is None:
-                died = int(line[idx])
-            elif len(who) < died:
-                who.append(int(line[idx]))
-            else:  # If we get here then we can calculate the next year payouts.
-                for i, age_group in enumerate(tt):
-                    alive_shares = 0
-                    alive_c = 0
-                    for j, person in enumerate(age_group):
-                        if len(person) == 2:  # Ignore our tracker at the end of the age_group list.
-                            continue
-                        if person[0] in who:
-                            if tt[i][len(age_group) - 1][1] > m:
-                                # Decrease the amount of alive people.
-                                tt[i][len(age_group) - 1][1] -= 1
-                                # Increase the dead shares tracker.
-                                tt[i][len(age_group) - 1][0] += person[2]
-                                # Set if their shares should count to false.
-                                tt[i][j][3] = False
-                            # Set them from alive to dead.
-                            tt[i][j][4] = False
-                        if person[3]:
-                            # We only want people whose shares should count to be added.
-                            alive_shares += person[2]
-                        if person[4]:
-                            # This is so we know if everyone is dead in the age_group.
-                            alive_c += 1
-                    if alive_c != 0:
-                        raw = ((age_group[len(age_group) - 1][0] * pct) / alive_shares) + pct
-                        print(f'{s} {i + 1} {math.trunc(int(raw * 1000) / 10) / 100:.2f}')
-                s += 1
-                pct, died, who = None, None, []
-                if len(line) != idx:
-                    pct = line[idx]
