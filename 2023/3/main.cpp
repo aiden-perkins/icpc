5a84f4e6ed86438a6505de862f69b6e409d0207f
aiden-perkins <perkinsaidenm@gmail.com>
2024-03-04 22:33:40 -0800

    small progress 2023 3

--- 2023/3/main.cpp
+++ 2023/3/main.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-struct Coord {
+struct Crd {
     int x;
     int y;
 };
 
 int main() {
-    string inputLine;
-    Coord end{};
+    Crd end{};
     int wormholeCount = 0;
-    vector<pair<Coord, Coord>> wormholes;
+    vector<vector<int>> wormholes;
+    string inputLine;
     while (getline(cin, inputLine)) {
         stringstream ss(inputLine);
         vector<int> line(istream_iterator<int>(ss), {});
@@ -19,20 +19,48 @@ int main() {
             end = {line[0], line[1]};
             wormholeCount = line[2];
         } else {
-            wormholes.push_back({{line[0], line[1]}, {line[2], line[3]}});
+            wormholes.push_back({line[0], line[1], line[2], line[3]});
         }
     }
-    cout << "end (x, y) are (" << end.x << ", " << end.y << "), wormhole count is " << wormholeCount << ", at: " << endl;
-    for (pair<Coord, Coord> wormhole: wormholes) {
-        cout << wormhole.first.x << ", " << wormhole.first.y << " to ";
-        cout << wormhole.second.x << ", " << wormhole.second.y;
+    // Create adjacency matrix
+    int vertexCount = (end.x + 1) * (end.y + 1);
+    int graph[vertexCount][vertexCount];
+    for (int i = 0; i < vertexCount; i++) {
+        for (int j = 0; j < vertexCount; j++) {
+            graph[i][j] = 0;
+        }
     }
-    // Plan:
-    // 1. Make a graph of the grid only
-    // 2. Connect the vertices that are wormholes
-    // 3. Once I have this completed graph I can use a path finding algorithm (dijkstra) to find the min distance.
-    // 4. Then I can run a BFS and eliminate distances that go above that min, and count the ones that reach the end.
-    // OR
-    // 3. I modify dijkstra to not stop when it finds the min and just keep a count of equal distances.
-    // I will probably try the later first but if it's too confusing then I'm sure the former will work.
+    for (int x = 0; x <= end.x; x++) {
+        for (int y = 0; y <= end.y; y++) {
+            int currentIndex = x * (end.y + 1) + y;
+            if (x + 1 <= end.x) {
+                int up = (x + 1) * (end.y + 1) + y;
+                graph[currentIndex][up] = 1;
+                graph[up][currentIndex] = 1;
+            }
+            if (y + 1 <= end.y) {
+                int left = x * (end.y + 1) + y + 1;
+                graph[currentIndex][left] = 1;
+                graph[left][currentIndex] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < wormholeCount; i++) {
+        int whStart = wormholes[i][0] * (end.y + 1) + wormholes[i][1];
+        int whEnd = wormholes[i][2] * (end.y + 1) + wormholes[i][3];
+        graph[whStart][whEnd] = 1;
+        graph[whEnd][whStart] = 1;
+    }
+    for (int i = 0; i < vertexCount; i++) {
+        cout << "index " << i << " has connections to ";
+        for (int j = 0; j < vertexCount; j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // Create a min distance list that will by default be inf
+    // create a final array that is a bool if we have found the shortest path
+    // Go until min distance is complete and there is the shortest path to all vertices
+    // get the next vertex by finding all that are infinite and are false in final array
+    // go through all possible edges and turn them from inf to a value
 }
