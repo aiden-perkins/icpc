7f38b7cb7b62202876b414b20db0498628cc2555
aiden-perkins <perkinsaidenm@gmail.com>
2024-03-04 23:07:30 -0800

    finish 2023 3 & fix c++.sh

--- 2023/3/main.cpp
+++ 2023/3/main.cpp
@@ -22,7 +22,8 @@ int main() {
             wormholes.push_back({line[0], line[1], line[2], line[3]});
         }
     }
-    // Create adjacency matrix
+    // Creating the adjacency matrix:
+    // Start with an empty graph
     int vertexCount = (end.x + 1) * (end.y + 1);
     int graph[vertexCount][vertexCount];
     for (int i = 0; i < vertexCount; i++) {
@@ -30,6 +31,7 @@ int main() {
             graph[i][j] = 0;
         }
     }
+    // Populate that graph with the known grid
     for (int x = 0; x <= end.x; x++) {
         for (int y = 0; y <= end.y; y++) {
             int currentIndex = x * (end.y + 1) + y;
@@ -45,22 +47,53 @@ int main() {
             }
         }
     }
+    // Add the wormholes to connect everything.
     for (int i = 0; i < wormholeCount; i++) {
         int whStart = wormholes[i][0] * (end.y + 1) + wormholes[i][1];
         int whEnd = wormholes[i][2] * (end.y + 1) + wormholes[i][3];
         graph[whStart][whEnd] = 1;
         graph[whEnd][whStart] = 1;
     }
+    // Create a min distance list that will hold the min distances to each vertex, INT_MAX by default.
+    int minDistance[vertexCount];
+    // Create a finished array that is full of false that represents if we have found the min path to that vertex.
+    int finished[vertexCount];
+    int minPathCount[vertexCount];
     for (int i = 0; i < vertexCount; i++) {
-        cout << "index " << i << " has connections to ";
-        for (int j = 0; j < vertexCount; j++) {
-            cout << graph[i][j] << " ";
-        }
-        cout << endl;
+        minDistance[i] = INT_MAX;
+        finished[i] = false;
+        minPathCount[i] = 1;
     }
-    // Create a min distance list that will by default be inf
-    // create a final array that is a bool if we have found the shortest path
-    // Go until min distance is complete and there is the shortest path to all vertices
-    // get the next vertex by finding all that are infinite and are false in final array
-    // go through all possible edges and turn them from inf to a value
+    minDistance[0] = 0;
+    // minPathCount[0] = 1;
+    // Loop enough times that finished becomes all true.
+    for (int _ = 0; _ < vertexCount; _++) {
+        // Get the next vertex by finding an index that is false in finished, and if there is more than one, find the min.
+        int currentIdx = -1;
+        int nextMinDistance = INT_MAX;
+        for (int i = 0; i < vertexCount; i++) {
+            if (minDistance[i] <= nextMinDistance && !finished[i]) {
+                nextMinDistance = minDistance[i];
+                currentIdx = i;
+            }
+        }
+        if (currentIdx == -1) {
+            throw runtime_error("finished is complete");
+        }
+        finished[currentIdx] = true;
+        // Go through all possible edges from the current vertex and update the correct things.
+        for (int i = 0; i < vertexCount; i++) {
+            // Check for unfinished neighbors of our current index.
+            if (!finished[i] && graph[currentIdx][i]) {
+                // Check if the new path is shorter than previously known.
+                if (minDistance[currentIdx] + 1 < minDistance[i]) {
+                    minDistance[i] = minDistance[currentIdx] + 1;
+                    minPathCount[i] = minPathCount[currentIdx];
+                } else if (minDistance[currentIdx] + 1 == minDistance[i]) {
+                    minPathCount[i] += minPathCount[currentIdx];
+                }
+            }
+        }
+    }
+    cout << minPathCount[vertexCount - 1] << endl;
 }
--- run_c++.sh
+++ run_c++.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+g++ "$1"/"$2"/main.cpp -o "$1"/"$2"/main.out
 if [ "$#" == 2 ]; then
     test_count=$(find "$1"/"$2"/ -type f -name "*.in" | wc -l)
     for i in $(seq $test_count); do
         echo Testing file "$i".in...
-        g++ "$1"/"$2"/main.cpp -o "$1"/"$2"/main
-        ./"$1"/"$2"/main < "$1"/"$2"/"$i".in > "$1"/"$2"/"$i".out
+        ./"$1"/"$2"/main.out < "$1"/"$2"/"$i".in > "$1"/"$2"/"$i".out
         diff "$1"/"$2"/"$i".out "$1"/"$2"/"$i".ans
         echo
     done
 else
     echo Testing file "$3".in...
-    g++ "$1"/"$2"/main.cpp -o "$1"/"$2"/main
-    ./"$1"/"$2"/main < "$1"/"$2"/"$3".in > "$1"/"$2"/"$3".out
+    ./"$1"/"$2"/main.out < "$1"/"$2"/"$3".in > "$1"/"$2"/"$3".out
     diff "$1"/"$2"/"$3".out "$1"/"$2"/"$3".ans
     echo
 fi
