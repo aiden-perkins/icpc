692024a29b794bcd9cf933c8f5366048fc198c3e
aiden-perkins <perkinsaidenm@gmail.com>
2024-03-12 13:45:39 -0700

    finally finish 2023 11
    Took asking on math stack exchange even though I asked my math major
    friends, oh well. Even sucks that in my notes I made an entire pascals
    triangle and couldn't connect it to binomial coefficients sadly, I was
    so close, at least I know for next time.

--- 2023/11/main.cpp
+++ 2023/11/main.cpp
@@ -2,9 +2,26 @@
 
 using namespace std;
 
+long int binomial_coefficient(int top, int bottom, long int** cache) {
+    if (bottom == 0 || bottom == top) {
+        cache[top][bottom] = 1;
+        return 1;
+    }
+    if (cache[top][bottom] != -1) {
+        return cache[top][bottom];
+    }
+    if (bottom == 1) {
+        cache[top][bottom] = top;
+        return top;
+    }
+    cache[top][bottom] = binomial_coefficient(top - 1, bottom - 1, cache) +
+        binomial_coefficient(top - 1, bottom, cache);
+    return cache[top][bottom];
+}
+
 int main() {
     cout << fixed;
-    cout << setprecision(6);
+    cout << setprecision(15);
     string inputLine;
     getline(cin, inputLine);
     int playerCount = stoi(inputLine);
@@ -17,85 +34,40 @@ int main() {
             scores[i][j] = line[j];
         }
     }
-
-    // First round is calculated a little differently.
-    double total = 0;
+    auto** cache = new long int*[playerCount];
     for (int i = 0; i < playerCount; i++) {
-        for (int j = i; j < playerCount; j++) {
-            total += scores[i][j];
-        }
-    }
-    total = total / ((double(playerCount * (playerCount - 1)) / 2) / (double(playerCount) / 2));
-
-    // Find out how large our numerator table needs to be.
-    int tournamentRounds = int(log2(playerCount));
-    int numeratorTableX = 2;
-    for (int i = 1; i < tournamentRounds; i++) {
-        numeratorTableX += (((i - 1) * i) / 2) + 1;
-    }
-    
-    // Fill out the numerator table.
-    int numeratorTable[numeratorTableX][playerCount];
-    for (int i = 0; i < numeratorTableX; i++) {
+        cache[i] = new long int[playerCount];
         for (int j = 0; j < playerCount; j++) {
-            if (!j || !i) {
-                numeratorTable[i][j] = 1;
-            } else {
-                numeratorTable[i][j] = numeratorTable[i - 1][j] + numeratorTable[i][j - 1];
+            cache[i][j] = -1;
+        }
+    }
+
+    long double total = 0;
+    int rounds = int(log2(playerCount));
+    for (int r = 1; r < rounds + 1; r++) {
+        int playersLeft = playerCount - (int(pow(2, r - 1)) - 1);
+        int playersMovingOn = playerCount - (int(pow(2, r)) - 1);
+        double pA = pow(2, r - 1) / (pow(2, rounds) - 1);
+        long int denominatorB = binomial_coefficient(int(pow(2, rounds) - 2), int(pow(2, r - 1) - 1), cache);
+        long int denominatorC = binomial_coefficient(
+            int(pow(2, rounds) - 1 - pow(2, r - 1)), int(pow(2, r - 1) - 1), cache
+        );
+        for (int i = 1; i < playersMovingOn + 1; i++) {
+            long int numeratorC = binomial_coefficient(
+                int(pow(2, rounds) - i - pow(2, r - 1)), int(pow(2, r - 1)) - 1, cache
+            );
+            for (int j = i + 1; j < playersLeft + 1; j++) {
+                long int numeratorB = binomial_coefficient(int(pow(2, rounds)) - j, int(pow(2, r - 1)) - 1, cache);
+                total += scores[i - 1][j - 1] * (pA * ((long double) numeratorB / denominatorB) * ((long double) numeratorC / denominatorC));
             }
         }
     }
-
-    // Fill out the probability table.
-    int posX = 0;
-    double probTable[tournamentRounds][playerCount];
-    for (int i = 0; i < tournamentRounds; i++) {
-        int playersLeft = playerCount - (int(pow(2, i)) - 1);
-        int denominator = numeratorTable[posX][playersLeft - 1];
-        for (int j = 0; j < playersLeft; j++) {
-            if (j < playersLeft) {
-                int numerator = numeratorTable[posX][playersLeft - j - 1];
-                probTable[i][j] = (double(numerator) / denominator);
-                if (i) {
-                    probTable[i][j] *= probTable[i - 1][j];
-                }
-            } else {
-                probTable[i][j] = 0;
-            }
-        }
-        posX += ((i * (i - 1)) / 2) + 1;
-    }
-
-    /*  This is just to make sure the probabilities are correct.
-    for (int i = 0; i < tournamentRounds; i++) {
-        for (int j = 0; j < playerCount; j++) {
-            cout << probTable[i][j] << " ";
-        }
-        cout << endl;
-    }
-    */
-
-    // Go through each possible pair in each round and add that to the total.
-    for (int i = 1; i < tournamentRounds; i++) {
-        int playersLeft = playerCount - (int(pow(2, i)) - 1);
-        int playersLost = playersLeft - (playerCount - (int(pow(2, i + 1)) - 1));
-        for (int j = 0; j < playersLeft - playersLost; j++) {
-            double p1m = probTable[i][j];
-            for (int k = j + 1; k < playersLeft; k++) {
-                double p2m = probTable[i][k];
-                int score = scores[j][k];
-                total += score * p1m * p2m;
-            }
-        }
-    }
-
-    // Here we are, this is supposed to be the correct answer, but I have made a fatal mistake. I was calculating the
-    // probability that a specific seed makes it to a specific round, generating all possible pairs in a specific
-    // round and then multiplying each seed's probability it made it there by each other in the pair, then multiplying
-    // that by the excitement score, and hoping that is how it works. This works for 1.in, because I calculate the first
-    // round correctly and the last round correctly, so when there are only 2 rounds, this code works. However, with 8
-    // or more, there are 3 or more rounds, thus my code is incorrect. In order to fix this I need to somehow calculate
-    // the probability a specific pair happens in a specific round, which I do not know how yet. :( I have wasted 20+
-    // hours on this, and I am hesitant to continue working on it again, maybe another time.
+    delete [] cache;
+    // https://math.stackexchange.com/questions/4878862/fixed-single-elimination-tournament-probabilities
+    // I gave up and asked this question, binomial coefficients and pascals triangle. It's funny because in my notebook
+    // and in my google sheets notes page, I had made an entire pascals triangle without even realizing, and when
+    // I asked some math major friends, they gave me formulas with factorials, but I was too stupid to realize that is
+    // a binomial coefficient, which I have learned before, and knew it had to do with probability, well at least I know
+    // now, hopefully next time I can solve this alone.
     cout << total << endl;
 }
