c15b9fe7d00dfde6741915870dcb0674ed2f7582
aiden-perkins <perkinsaidenm@gmail.com>
2024-03-20 01:47:55 -0700

    Used guassian elimination to solve Mixtures
    - To do goldstein/mixtures you just solve the ratios as a system of linear equations, its that simple. To program this I had to learn the guassian elimination algorithm, to get it to echelon row form, and then continue to reduced echelon row form, which gave me my answers.
    - Moved all of the comments to my sheets because I feel as if they don't belong here.

---
+++ goldstein/Mixtures/main.py
@@ -0,0 +1,62 @@
+import sys
+
+goal = tuple([int(a) for a in input().split(':')])
+ratios = []
+for line in sys.stdin:
+    ratio = []
+    for chemical in line.split(':'):
+        if chemical == '':
+            ratio.append(0)
+        else:
+            ratio.append(int(chemical))
+    ratios.append(ratio)
+
+ratio_count = len(ratios) + 1
+equation_count = len(ratios)
+matrix = [[0 for _ in range(ratio_count)] for _ in range(equation_count)]
+for y in range(ratio_count - 1):
+    for x in range(equation_count):
+        matrix[y][x] = ratios[x][y]
+    matrix[y][-1] = goal[y]
+
+# Use gaussian elimination to get to echelon form:
+# leading coefficient is 1
+# Each leading coefficient is in a column to the right of the one above it
+# Rows with more leading zeroes go after the ones with less
+for i in range(equation_count):
+    matrix[i:] = sorted(matrix[i:])
+    leading_coefficient = 0
+    for m, v in enumerate(matrix[i]):
+        if v != 0:
+            leading_coefficient = m
+            break
+    if matrix[i][leading_coefficient] != 1:
+        reciprocal = 1 / matrix[i][leading_coefficient]
+        for j in range(leading_coefficient, ratio_count):
+            matrix[i][j] *= reciprocal
+    for k in range(i + 1, equation_count):
+        multiplicant = matrix[k][leading_coefficient]
+        for j in range(leading_coefficient, ratio_count):
+            matrix[k][j] = matrix[k][j] - (matrix[i][j] * multiplicant)
+
+# After we have it in echelon form we want to turn it into the reduced row echelon form:
+# The leading coefficient in each row is the only non-zero item
+for i in range(equation_count - 1):
+    start = 0
+    for m, v in enumerate(matrix[i]):
+        if v == 1:
+            start = m + 1
+            break
+    for j in range(start, ratio_count - 1):
+        multiplicant = matrix[i][j]
+        for k in range(j, ratio_count):
+            matrix[i][k] = matrix[i][k] - (multiplicant * matrix[j][k])
+
+# Once we have it in reduced row echelon form, the rightmost element in each row is the answer.
+answers = [a[-1] for a in matrix]
+answers = [round((1 / min(answers)) * a) for a in answers]
+if min(answers) < 0:  # Maybe < 1?
+    print(-1)
+else:
+    for ans in answers:
+        print(ans)
